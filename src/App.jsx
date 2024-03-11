1b520a6c778e635e735c2f510c7e05c0c151ac6c
Jhony Souza <jhony.jss@gmail.com>
2024-03-11 11:43:10 -0300

    gohelp version 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import "react-alice-carousel/lib/alice-carousel.css";
+import { useMemo } from "react";
 
 function App() {
   const [isFlipped, setFlipped] = useState(false);
+  const [isShadow, setShadow] = useState(true);
   const [deferredPrompt, setDeferredPrompt] = useState(null);
   const [isInstalled, setIsInstalled] = useState(false);
   const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
@@ -57,16 +59,27 @@ function App() {
     // Add more card data as needed
   ];
 
+  useEffect(() => {
+    setShadow(false);
+
+    const timeoutId = setTimeout(() => {
+      setShadow(true);
+    }, 450);
+
+    return () => clearTimeout(timeoutId);
+  }, [isFlipped]);
+
   return (
     <>
       <section className="container mx-auto px-4 h-screen flex-center flex-col">
-        <div className="max-w-[320px] carousel rounded-box shadow-custom">
+        {}
+        <div className={`max-w-[320px] carousel rounded-box ${isShadow ? "shadow-custom transition" : ""}`}>
           {cardData.map((card) => (
-            <div className="carousel-item w-full" key={`${card}-carousel`}>
+            <div className="carousel-item w-full" key={`${card.id}-carousel`}>
               <ReactCardFlip key={card.id} isFlipped={isFlipped} flipDirection="horizontal">
                 {/* Front side */}
                 <div
-                  className="flex justify-center bg-white min-h-[399px] w-full rounded-2xl overflow-hidden shadow-custom"
+                  className="flex justify-center bg-white min-h-[399px] w-full rounded-2xl overflow-hidden"
                   onClick={() => setFlipped(!isFlipped)}
                 >
                   <img src={card.backImage} className="w-full max-h-full object-cover" alt="" />
@@ -74,7 +87,7 @@ function App() {
 
                 {/* Back side */}
                 <div
-                  className="flex justify-center bg-white min-h-[399px] w-full rounded-2xl overflow-hidden shadow-custom"
+                  className="flex justify-center bg-white min-h-[399px] w-full rounded-2xl overflow-hidden"
                   onClick={() => setFlipped(!isFlipped)}
                 >
                   <img src={card.frontImage} className="w-full max-h-full object-cover" alt="" />
